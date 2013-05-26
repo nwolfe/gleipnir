fa0f34ca6c449b092769a03f4b4814894d1bf56f
Nate Wolfe <nwolfe@puppetlabs.com>
2013-05-26 02:45:23 -0700

    Update player art, started first level

--- gleipnir.rb
+++ gleipnir.rb
@@ -16,7 +16,7 @@ class Intro < Chingu::GameState
     Chingu::Text.create(:text => "Programming by Nate Wolfe", :x => 350, :y => 150)
     Chingu::Text.create(:text => "Artwork by Lew Lewis", :x => 365, :y => 170)
     Chingu::Text.create(:text => "PRESS S TO START", :x => 370, :y => 400)
-    self.input = {:s => Play}
+    self.input = {:s => Play, :escape => :exit}
   end
 end
 
@@ -24,12 +24,65 @@ class Play < Chingu::GameState
   def initialize(options = {})
     super
     self.input = {:escape => :exit}
+    #load_game_objects # Load objects from "play.yml"
+    create_level
     @player = Player.create(:x => 200, :y => 200)
+  end
+
+  def create_level
+    # left wall
+    for i in 0..15
+      x = 64
+      y = 64 + (32*i)
+      Wall.create(:x => x, :y => y) 
+    end
+    # right wall
+    for i in 0..15
+      x = 64 + (32*21)
+      y = 64 + (32*i)
+      Wall.create(:x => x, :y => y) 
+    end
+    # top wall
+    for i in 1..21
+      x = 64 + (32*i)
+      y = 64
+      Wall.create(:x => x, :y => y) 
+    end
+    # bottom wall
+    for i in 1..21
+      x = 64 + (32*i)
+      y = 64 + (32*15)
+      Wall.create(:x => x, :y => y) 
+    end
+
+    # Fill with Grass
+    for i in 0..19
+      for j in 0..13
+        x = 96 + (32*i)
+        y = 96 + (32*j)
+        Grass.create(:x => x, :y => y)
+      end
+    end
   end
 end
 
+class Grass < Chingu::GameObject
+  def setup
+    @image = Image["grass.png"]
+  end
+end
+
+class Wall < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["dark_brick.png"]
+  end
+end
 
 class Player < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
   def setup
     self.input = {
       [:holding_up, :holding_k] => :move_up,
@@ -45,19 +98,26 @@ class Player < Chingu::GameObject
       [:released_right, :released_l] => :halt_right
     }
 
-    @animations = Chingu::Animation.new(:file => "sprite_sheet_32x32.png")
+    @animations = Chingu::Animation.new(:file => "player_sheet2_32x32.png")
     @animations.frame_names = {
       :down => 0..2, 
       :up => 3..5, 
-      :left => 6..8, 
-      :right => 9..11
+      :right => 6..8, 
+      :left => 9..11
     }
     @image = @animations[:down].next
+    update
+  end
+
+  def update
+    @last_x = @x
+    @last_y = @y
   end
 
   def move_left
     @x -= 3
     @image = @animations[:left].next
+    @x = @last_x if self.first_collision(Wall)
   end
 
   def halt_left
@@ -67,6 +127,7 @@ class Player < Chingu::GameObject
   def move_right
     @x += 3
     @image = @animations[:right].next
+    @x = @last_x if self.first_collision(Wall)
   end
 
   def halt_right
@@ -76,6 +137,7 @@ class Player < Chingu::GameObject
   def move_up
     @y -= 3
     @image = @animations[:up].next
+    @y = @last_y if self.first_collision(Wall)
   end
 
   def halt_up
@@ -85,6 +147,7 @@ class Player < Chingu::GameObject
   def move_down
     @y += 3
     @image = @animations[:down].next
+    @y = @last_y if self.first_collision(Wall)
   end
 
   def halt_down
