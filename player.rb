d777e1eb9d8ae85ae4d1e7dd084291f432390b66
Nate Wolfe <nwolfe@puppetlabs.com>
2013-06-03 22:16:19 -0700

    Pulled classes out into their own files

---
+++ player.rb
@@ -0,0 +1,94 @@
+class Player < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    self.input = {
+      [:holding_up, :holding_k] => :move_up,
+      [:released_up, :released_k] => :halt_up,
+
+      [:holding_down, :holding_j] => :move_down,
+      [:released_down, :released_j] => :halt_down,
+
+      [:holding_left, :holding_h] => :move_left,
+      [:released_left, :released_h] => :halt_left,
+        
+      [:holding_right, :holding_l] => :move_right,
+      [:released_right, :released_l] => :halt_right
+    }
+
+    @animations = Chingu::Animation.new(:file => "player/player_sheet2_32x32.png")
+    @animations.frame_names = {
+      :down => 0..2, 
+      :up => 3..5, 
+      :right => 6..8, 
+      :left => 9..11
+    }
+    @image = @animations[:down].next
+    update
+  end
+
+  def update
+    @last_x = @x
+    @last_y = @y
+  end
+
+  def kollides?
+    return self.first_collision(DarkBrick) || 
+    self.first_collision(BushyTree) || 
+    self.first_collision(BushyTreeApples) || 
+    self.first_collision(WillowTree)
+    self.first_collision(RockWall) || 
+    self.first_collision(CaveWall) || 
+    self.first_collision(CaveWall2) || 
+    self.first_collision(CaveWallGold) || 
+    self.first_collision(CaveWallShade)
+  end
+
+  def move(x, y)
+    if x > 0 || x < 0
+      @x += x
+      @x = @last_x if kollides?
+    end
+
+    if y > 0 || y < 0
+      @y += y
+      @y = @last_y if kollides?
+    end
+  end
+
+  def move_left
+    @image = @animations[:left].next
+    move(-2, 0)
+  end
+
+  def halt_left
+    @image = @animations[:left].first
+  end
+
+  def move_right
+    @image = @animations[:right].next
+    move(2, 0)
+  end
+
+  def halt_right
+    @image = @animations[:right].first
+  end
+
+  def move_up
+    @image = @animations[:up].next
+    move(0, -2)
+  end
+
+  def halt_up
+    @image = @animations[:up].first
+  end
+
+  def move_down
+    @image = @animations[:down].next
+    move(0, 2)
+  end
+
+  def halt_down
+    @image = @animations[:down].first
+  end
+end
---
+++ tiles.rb
@@ -0,0 +1,133 @@
+class Dirt < Chingu::GameObject
+  def setup
+    @image = Image["floor/dirt.png"]
+  end
+end
+
+class Grass < Chingu::GameObject
+  def setup
+    @image = Image["floor/grass.png"]
+  end
+end
+
+class GrassRocks < Chingu::GameObject
+  def setup
+    @image = Image["floor/grass_rocks.png"]
+  end
+end
+
+class Gravel < Chingu::GameObject
+  def setup
+    @image = Image["floor/gravel.png"]
+  end
+end
+
+class MossGround < Chingu::GameObject
+  def setup
+    @image = Image["floor/moss_ground.png"]
+  end
+end
+
+class MossStone < Chingu::GameObject
+  def setup
+    @image = Image["floor/moss_stone.png"]
+  end
+end
+
+class Mud < Chingu::GameObject
+  def setup
+    @image = Image["floor/mud.png"]
+  end
+end
+
+class MudFloor < Chingu::GameObject
+  def setup
+    @image = Image["floor/mud_floor.png"]
+  end
+end
+
+class MudFloorGold < Chingu::GameObject
+  def setup
+    @image = Image["floor/mud_floor_gold.png"]
+  end
+end
+
+class RockPath < Chingu::GameObject
+  def setup
+    @image = Image["floor/rock_path.png"]
+  end
+end
+
+## WALL TILES
+  
+class DarkBrick < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["wall/dark_brick.png"]
+  end
+end
+
+class BushyTree < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["terrain/bushy_tree.png"]
+  end
+end
+
+class BushyTreeApples < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["terrain/bushy_tree_apples.png"]
+  end
+end
+
+class WillowTree < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["terrain/willow.png"]
+  end
+end
+
+class RockWall < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["wall/rock_wall.png"]
+  end
+end
+
+class CaveWall < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["wall/cave_wall.png"]
+  end
+end
+
+class CaveWall2 < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["wall/cave_wall_2.png"]
+  end
+end
+
+class CaveWallGold < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["wall/cave_wall_gold.png"]
+  end
+end
+
+class CaveWallShade < Chingu::GameObject
+  traits :bounding_box, :collision_detection
+
+  def setup
+    @image = Image["wall/cave_wall_shade.png"]
+  end
+end
