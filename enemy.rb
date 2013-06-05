4fe69070f4f07aff8d8ae645c4110e01e3590fc0
Nate Wolfe <nwolfe@puppetlabs.com>
2013-06-04 23:38:41 -0700

    test

--- enemy.rb
+++ enemy.rb
@@ -2,7 +2,7 @@ require './player.rb'
 
 class Enemy < Chingu::GameObject
   trait :collision_detection
-  trait :bounding_box, :scale => [0.60, 0.80]
+  trait :bounding_box, :debug => true#, :scale => [0.60, 0.80]
 
   def setup
     @image = Chingu::Animation.new(:file => "enemy_sheet_32x32.png")[rand(6)]
@@ -14,11 +14,8 @@ class Enemy < Chingu::GameObject
   end
 
   def take_damage(amount)
-    @life = @life - amount
     puts "You hit for #{amount}!"
-  end
-
-  def dead?
-    @life <= 0
+    @life -= amount
+    destroy if @life <= 0
   end
 end
