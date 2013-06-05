a5568f95bce69c486ba038ccadec2ac4b150cd56
Nate Wolfe <nwolfe@puppetlabs.com>
2013-06-05 01:06:07 -0700

    Major enemy enhancements

--- enemy.rb
+++ enemy.rb
@@ -1,8 +1,8 @@
 require './player.rb'
 
 class Enemy < Chingu::GameObject
-  trait :collision_detection
-  trait :bounding_box, :debug => true#, :scale => [0.60, 0.80]
+  traits :collision_detection, :timer
+  trait :bounding_box, :scale => [0.60, 0.80]
 
   def setup
     @image = Chingu::Animation.new(:file => "enemy_sheet_32x32.png")[rand(6)]
@@ -14,8 +14,10 @@ class Enemy < Chingu::GameObject
   end
 
   def take_damage(amount)
-    puts "You hit for #{amount}!"
     @life -= amount
+    self.mode = :additive
+    after(50) { self.mode = :default }
+    puts "You hit for #{amount}! (#{@life})"
     destroy if @life <= 0
   end
 end
