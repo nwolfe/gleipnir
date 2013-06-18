ce1bf23696bc51c90b6f6f62f490ba674d00be0b
Nate Wolfe <nwolfe@puppetlabs.com>
2013-06-17 22:56:43 -0700

    Switch instead of push

--- gleipnir.rb
+++ gleipnir.rb
@@ -6,7 +6,7 @@ require './starting_zone.rb'
 class Gleipnir < Chingu::Window
   def setup
     self.caption = "Gleipnir!"
-    push_game_state StartingZone
+    switch_game_state StartingZone
   end
 end
 
